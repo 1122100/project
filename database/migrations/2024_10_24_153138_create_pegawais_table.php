3c2d0856d51bf383741ba18ffc59573473f579c0
twelvekun <sadadguci@gmail.com>
2024-11-14 21:28:18 +0700

    tes

--- database/migrations/2024_10_24_153138_create_pegawais_table.php
+++ database/migrations/2024_10_24_153138_create_pegawais_table.php
@@ -12,7 +12,7 @@ return new class extends Migration
     public function up(): void
     {
         Schema::create('pegawais', function (Blueprint $table) {
-            $table->id();
+            $table->id('pegawai_id');
             $table->timestamps();
         });
     }
